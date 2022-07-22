45286fa4adc577323769568fa16e32ec2b67b24b
anclaro <anclaro@iu.edu>
2022-07-22 12:46:02 -0400

    plotweightsbiases will plot all weights/biases

--- revision/visdata.py
+++ revision/visdata.py
@@ -11,26 +11,30 @@ data = np.load("./data/noreward.npz")
 
 
 
-def plotWeightsBiases(data, show=False):
+def plotWeightsBiases(data, show=False, legend=True):
     cmap = plt.get_cmap('tab20').colors
     fig, ax = plt.subplots(figsize=(8,4))
     time = np.arange(0, data['duration'], 0.1)
-    ax.plot(time, data['extended_weights'].T[0,0],         color=cmap[0], label='w_00')
-    ax.plot(time, data['inner_weights'].T[0,0],                 color=cmap[1 ])
-    ax.plot(time, data['extended_weights'].T[0,1],         color=cmap[2], label='w_01')
-    ax.plot(time, data['inner_weights'].T[0,1],                 color=cmap[3 ])
-    ax.plot(time, data['extended_weights'].T[1,0],         color=cmap[4], label='w_10')
-    ax.plot(time, data['inner_weights'].T[1,0],                 color=cmap[5 ])
-    ax.plot(time, data['extended_weights'].T[1,1],         color=cmap[6], label='w_11')
-    ax.plot(time, data['inner_weights'].T[1,1],                 color=cmap[7 ])
-    ax.plot(time, data['biases'].T[0], color=cmap[8], label='bias_0')
-    ax.plot(time, data['extended_biases'].T[0], color=cmap[9])
-    ax.plot(time, data['biases'].T[1], color=cmap[10], label="bias_1")
-    ax.plot(time, data['extended_biases'].T[1], color=cmap[11])
+    for i in range(data["size"]):
+        for j in range(data['size']):
+            ax.plot(time, data['extended_weights'].T[i,j],         color=cmap[i], label=f'w_{i}{j}')
+            ax.plot(time, data['inner_weights'].T[i,j],                 color=cmap[i+1])
+
+        ax.plot(time, data['biases'].T[i], color=cmap[i], ls='dotted', label='bias_0')
+        ax.plot(time, data['extended_biases'].T[i], ls='dotted', color=cmap[9])
+#    ax.plot(time, data['extended_weights'].T[0,1],         color=cmap[2], label='w_01')
+#    ax.plot(time, data['inner_weights'].T[0,1],                 color=cmap[3 ])
+#    ax.plot(time, data['extended_weights'].T[1,0],         color=cmap[4], label='w_10')
+#    ax.plot(time, data['inner_weights'].T[1,0],                 color=cmap[5 ])
+#    ax.plot(time, data['extended_weights'].T[1,1],         color=cmap[6], label='w_11')
+#    ax.plot(time, data['inner_weights'].T[1,1],                 color=cmap[7 ])
+#    ax.plot(time, data['biases'].T[1], color=cmap[10], label="bias_1")
+#    ax.plot(time, data['extended_biases'].T[1], color=cmap[11])
     ax.axvline(data['learning_start']*data['stepsize'], color='k', lw='1')
     ax.title.set_text(f"Weight and Bias change during Trial:{generator_type}")
     if show:
-        plt.legend()
+        if legend:
+            plt.legend()
         plt.savefig("./data/images/weight-bias.png")
         plt.show()
 def plotPerformance(data, show=False):
